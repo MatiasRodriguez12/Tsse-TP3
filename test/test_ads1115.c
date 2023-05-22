7d93ad0934ac2354fa0f0df2c0084b7d70a41976
MatiasRodriguez12 <matiasrodriguez552@gmail.com>
2023-05-22 02:55:05 -0300

    Se actualizan comentarios y nombre de funcion auxiliar

--- test/test_ads1115.c
+++ test/test_ads1115.c
@@ -3,7 +3,7 @@
 #include "mock_ADS1115_port.h"
 
 //--------------------Funciones auxiliares--------------------
-bool auxiliar_ads_transmit(uint8_t slaveAddres, uint8_t *word,uint16_t nroBytes) {
+bool auxiliar_ads1115_transmit(uint8_t slaveAddres, uint8_t *word,uint16_t nroBytes) {
    return FUNCTION_OK;
 }
 
@@ -18,10 +18,10 @@ void test_iniciar_canal_modo_single(void) {
     TEST_ASSERT_EQUAL(SINGLE_MODE_A0,channelCero.channel);
 }
 
-//Inicializar configuración de canales A1 en modo single - lectura por consulta
+//Inicializar configuración de canal A1 en modo single - lectura por consulta
 void test_iniciar_canal_modo_diferential(void){
     signalADS1115 channelUno;
 
-    ADS1115_Transmit_fake.custom_fake = auxiliar_ads_transmit;
+    ADS1115_Transmit_fake.custom_fake = auxiliar_ads1115_transmit;
     TEST_ASSERT_TRUE(ADS1115_channelInitPolled(&channelUno,SINGLE_MODE_A1,SLAVE_ADRRES_GND));
 }
